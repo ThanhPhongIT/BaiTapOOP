d27f2619c759eb2eff15c705decf6db2fb489664
{phong} <{phonga3bdq@gmail.com}>
2020-04-11 14:50:50 +0700

    bai 5.5 update

--- BAI5.5_PHAMVANPHONG.cpp
+++ BAI5.5_PHAMVANPHONG.cpp
@@ -3,51 +3,6 @@
 #include<iomanip>
 #include<math.h>
 using namespace std;
-
-class MayGiat{
-private:
-	char Brand[30];
-	float CongSuat;
-	long giaTien;
-	int dungTich;
-public:
-	void nhap();
-	void xuat();
-};
-void MayGiat::nhap(){
-	cout<<"Nhap vao thuong hieu: ";		fflush(stdin);		gets(Brand);
-	cout<<"Nhap vao cong suat: ";		cin>>CongSuat;
-	cout<<"Nhap vao gia tien: ";		cin>>giaTien;
-	cout<<"Nhap vao dung tich: ";		cin>>dungTich;
-}
-void MayGiat::xuat(){
-	cout<<setw(30)<<Brand;
-	cout<<setw(12)<<CongSuat;
-	cout<<setw(12)<<giaTien;
-	cout<<setw(12)<<dungTich<<endl;
-}
-class TuLanh{
-private:
-	char Brand[30];
-	float CongSuat;
-	long giaTien;
-	int khoiLuongGiat;
-public:
-	void nhap();
-	void xuat();
-};
-void TuLanh::nhap(){
-	cout<<"Nhap vao thuong hieu: ";		fflush(stdin);		gets(Brand);
-	cout<<"Nhap vao cong suat: ";		cin>>CongSuat;
-	cout<<"Nhap vao gia tien: ";		cin>>giaTien;
-	cout<<"Nhap vao dung tich: ";		cin>>khoiLuongGiat;
-}
-void TuLanh::xuat(){
-	cout<<setw(30)<<Brand;
-	cout<<setw(12)<<CongSuat;
-	cout<<setw(12)<<giaTien;
-	cout<<setw(12)<<khoiLuongGiat<<endl;
-}
 class ThucPham{
 protected:
 	char TenThucPham[30];
@@ -95,22 +50,7 @@ void TPKHOP::xuat(){
 	cout<<setw(12)<<khoiLuong<<endl;
 }
 int main(){
-	MayGiat a;
-	TuLanh b;
-	cout<<"Nhap vao may giat a\n";
-	a.nhap();
-	cout<<setw(30)<<"Brand";
-	cout<<setw(12)<<"CongSuat";
-	cout<<setw(12)<<"giaTien";
-	cout<<setw(12)<<"dungTich"<<endl;
-	a.xuat();
-	cout<<"Nhap vao tu lanh b\n";
-	b.nhap();
-	cout<<setw(30)<<"Brand";
-	cout<<setw(12)<<"CongSuat";
-	cout<<setw(12)<<"giaTien";
-	cout<<setw(12)<<"KhoiLuong"<<endl;
-	b.xuat();
+
 	TPHOP x;
 	TPKHOP y;
 	cout<<"Nhap vao thuc pham hop\n";
